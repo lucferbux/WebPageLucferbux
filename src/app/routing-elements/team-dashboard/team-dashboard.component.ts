1ea69aab383a6709ad2a7396c9361a91d291c41a
lfernandezaragon <lucasfernandezaragon@gmail.com>
2019-08-19 11:12:06 +0200

    updated description

--- src/app/routing-elements/team-dashboard/team-dashboard.component.ts
+++ src/app/routing-elements/team-dashboard/team-dashboard.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { Team, TeamId } from '../../model/team';
+import { TeamId } from '../../model/team';
 import { FirebaseService } from '../../firebase.service';
 import { Router } from '@angular/router';
 import { EditDataService } from '../edit-data.service';
@@ -18,15 +16,8 @@ export class TeamDashboardComponent {
   rowHeight: String = "185px";
   isLarge: Boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver, private tfs: FirebaseService, private editService: EditDataService, private router: Router, public auth: AuthService ) {
+  constructor(private tfs: FirebaseService, private editService: EditDataService, private router: Router, public auth: AuthService ) {
     this.getTeam();
-    this.breakpointObserver.observe(['(max-width: 369px)']).subscribe(result => { if(result.matches){ this.updateInterface("160px", false)}})
-    this.breakpointObserver.observe(['(min-width: 370px) and (max-width: 599px)']).subscribe(result => { if(result.matches){ this.updateInterface("140px", false)}})
-    this.breakpointObserver.observe([Breakpoints.Small]).subscribe(result => {   if(result.matches){ this.updateInterface("110px", false)}})
-    this.breakpointObserver.observe([Breakpoints.Medium]).subscribe(result => { if(result.matches){ this.updateInterface("150px", true)}})
-    this.breakpointObserver.observe([Breakpoints.Large]).subscribe(result => { if(result.matches){ this.updateInterface("130px", true)}})
-    this.breakpointObserver.observe([Breakpoints.XLarge]).subscribe(result => { if(result.matches){ this.updateInterface("120px", true)}})
-
   }
   
   getTeam() : void {
@@ -41,7 +32,6 @@ export class TeamDashboardComponent {
   }
 
   editTeam(member: TeamId) {
-    //Update member
     this.editService.editTeamSource(member);
     this.router.navigate(["/edit/team"]);
   }
